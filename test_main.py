2d6bad44963750494c954dbcb0ba84a350996793
Khodane <khodaninditwani15@gmail.com>
2025-06-15 13:25:18 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,6 +1,7 @@
 # test_main.py
+import pytest
 from main import greet
 
 
 def test_greet():
-    assert greet() == "Hello, CI!"
+    assert greet() == "Goodbye, CI" # This will fail because greet() returns "Hello, CI"
