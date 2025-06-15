5b9e3cd76ed421e09b28cca50dbc1b1648c93dbe
khodane <khodaninditwani15@gmail.com>
2025-06-15 13:32:11 +0200

    fix brocken test

--- test_main.py
+++ test_main.py
@@ -1,14 +1,7 @@
 # test_main.py
-<<<<<<< HEAD
-from main import greet
-
-def test_greet():
-    assert greet() == "Hello, CI!"
-=======
 import pytest
 from main import greet
 
 
 def test_greet():
-    assert greet() == "Goodbye, CI" # This will fail because greet() returns "Hello, CI"
->>>>>>> 2d6bad44963750494c954dbcb0ba84a350996793
+    assert greet() == "Hello, CI"
